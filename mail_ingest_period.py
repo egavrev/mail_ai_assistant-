17e0f3ea6a0d0e71efca7d99c418a8ae9cae34a8
Eugeniu Gavrev <gavreveugen@gmail.com>
2025-06-10 23:20:29 +0300

    initial commit with main files to develop

---
+++ mail_ingest_period.py
@@ -0,0 +1,115 @@
+import argparse
+import asyncio
+from typing import Optional
+from eaia.gmail import fetch_group_emails
+from eaia.main.config import get_config
+from langgraph_sdk import get_client
+import httpx
+import uuid
+import hashlib
+from eaia.graph_processor import graph_processor
+#####
+# This script is used to ingest emails from a given email address for a given period of time.
+# period is defining in days
+async def main(
+    url: Optional[str] = None,
+    start_date: datetime.datetime = datetime.datetime.now() - datetime.timedelta(days=30),
+    end_date: datetime.datetime = datetime.datetime.now(),
+    gmail_token: Optional[str] = None,
+    gmail_secret: Optional[str] = None,
+    early: bool = True,
+    rerun: bool = False,
+    email: Optional[str] = None,
+    period: int = 1,
+):
+    if email is None:
+        email_address = get_config({"configurable": {}})["email"]
+    else:
+        email_address = email
+    
+    #TODO 1: revuild fetch_group_emails to fetch emails for a given period
+    for email in fetch_group_emails(
+        email_address,
+        minutes_since=minutes_since,
+        gmail_token=gmail_token,
+        gmail_secret=gmail_secret,
+    ):
+        thread_id = str(
+            uuid.UUID(hex=hashlib.md5(email["thread_id"].encode("UTF-8")).hexdigest())
+        )
+        #TODO 2: add graph call
+        result = await graph_processor.ainvoke(email)
+        if "user_respond" in email:
+            await client.threads.update_state(thread_id, None, as_node="__end__")
+            continue
+        recent_email = thread_info["metadata"].get("email_id")
+        if recent_email == email["id"]:
+            if early:
+                break
+            else:
+                if rerun:
+                    pass
+                else:
+                    continue
+        await client.threads.update(thread_id, metadata={"email_id": email["id"]})
+
+        
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--url",
+        type=str,
+        default=None,
+        help="URL to run against",
+    )
+    parser.add_argument(
+        "--early",
+        type=int,
+        default=1,
+        help="whether to break when encountering seen emails",
+    )
+    parser.add_argument(
+        "--rerun",
+        type=int,
+        default=0,
+        help="whether to rerun all emails",
+    )
+    parser.add_argument(
+        "--minutes-since",
+        type=int,
+        default=60,
+        help="Only process emails that are less than this many minutes old.",
+    )
+    parser.add_argument(
+        "--gmail-token",
+        type=str,
+        default=None,
+        help="The token to use in communicating with the Gmail API.",
+    )
+    parser.add_argument(
+        "--gmail-secret",
+        type=str,
+        default=None,
+        help="The creds to use in communicating with the Gmail API.",
+    )
+    parser.add_argument(
+        "--email",
+        type=str,
+        default=None,
+        help="The email address to use",
+    )
+
+    args = parser.parse_args()
+    asyncio.run(
+        main(
+            url=args.url,
+            minutes_since=args.minutes_since,
+            gmail_token=args.gmail_token,
+            gmail_secret=args.gmail_secret,
+            early=bool(args.early),
+            rerun=bool(args.rerun),
+            email=args.email,
+        )
+    )
